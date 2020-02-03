2c9dc8f945803e8fe3051bf16cea83b186aed3be
Gabe Coelho <gabrielpalmacoelho@gmail.com>
2020-02-03 10:41:20 -0700

    finished array pair sum

---
+++ 2.array-sequences/anagram_check.py
@@ -0,0 +1,59 @@
+"""
+Anagram Check
+Problem
+Given two strings, check to see if they are anagrams. An anagram is when the two strings can be written using the exact same letters (so you can just rearrange the letters to get a different phrase or word).
+
+For example:
+
+"public relations" is an anagram of "crap built on lies."
+
+"clint eastwood" is an anagram of "old west action"
+
+Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g".
+"""
+from nose.tools import assert_equal
+
+
+def anagram(str1, str2):
+
+    str1 = str1.replace(' ', '').lower()
+    str2 = str2.replace(' ', '').lower()
+
+    if len(str1) != len(str2):
+        return False
+
+    count = {}
+
+    for letter in str1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    for letter in str2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+    return True
+
+
+class AnagramTest(object):
+
+    def test(self, sol):
+        assert_equal(sol('go go go', 'gggooo'), True)
+        assert_equal(sol('abc', 'cba'), True)
+        assert_equal(sol('hi man', 'hi     man'), True)
+        assert_equal(sol('aabbcc', 'aabbc'), False)
+        assert_equal(sol('123', '1 2'), False)
+        print("ALL TEST CASES PASSED")
+
+
+# Run Tests
+t = AnagramTest()
+t.test(anagram)
